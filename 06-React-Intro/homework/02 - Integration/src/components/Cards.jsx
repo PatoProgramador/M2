52197638797f910a60a4fb7ac2b52edb4e0d994d
Pato Programador <spartan117947@hotmail.com>
2022-11-29 18:53:42 -0500

    Implementacion del primer paso del proyecto integrador

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,21 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+
+   return (
+      <div>
+         {
+            characters.map(({name,species,gender,image}, index)=>{
+               return <Card 
+                  key={index}
+                  name ={name}
+                  species={species}
+                  gender={gender}
+                  image={image}
+                  onClose={()=> window.alert("Emulamos que se cierra la card")}
+               />
+            })
+         }
+      </div>
+   );
 }
--- 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 export default function SearchBar(props) {
    return (
       <div>
-         {/* <input type='search' />
-      <button onClick={}>Agregar</button> */}
+         <input type='search' />
+         <button onClick={()=>{props.onSearch(5)}}>Agregar</button>
       </div>
    );
 }
