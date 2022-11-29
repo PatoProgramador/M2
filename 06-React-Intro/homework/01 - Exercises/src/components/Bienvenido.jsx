cf32ed41fe1688cdfe6495addaadbeaa1059601a
Pato Programador <spartan117947@hotmail.com>
2022-11-29 18:02:44 -0500

    Uso de componentes y map en react

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Pato";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Solución de la primera Homework de React</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {
+          techSkills.map((str,index)=><li key={index}>{str}</li>)
+        }
+      </ul>
+      <Botones alerts= {alerts}/>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Botones extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={() =>{alert(this.props.alerts.m1)}}>Módulo 1</button>
+                <button onClick={() =>{alert(this.props.alerts.m2)}}>Módulo 2</button>
+            </div>
+        )
+    }
+}
+
+export default Botones;
