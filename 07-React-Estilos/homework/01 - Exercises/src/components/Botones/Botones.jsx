5764ae8ae907cce4f6e07aee2d9fc4c6fe8a7a92
Pato Programador <spartan117947@hotmail.com>
2022-12-06 01:04:40 -0500

    Homework para realizar diferentes formas de aplicar estilos

--- 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
+++ 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import Styled from "styled-components";
 
-
-const DivButtons = 'DivButtons'
-const Buttons = 'Buttons'
+const DivButtons = Styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`
+const Buttons = Styled.button`
+  border-radius: 5px;
+  margin: 10px;
+  padding: 5px;
+`
 
 export default class Botones extends React.Component {
   render () {
     const { alerts } = this.props
     return (
-      <div>
-        <button onClick={() => window.alert(alerts.m1)}>Módulo 1</button>
-        <button onClick={() => window.alert(alerts.m2)}>Módulo 2</button>
-      </div>
+      <DivButtons>
+        <Buttons onClick={() => window.alert(alerts.m1)}>Módulo 1</Buttons>
+        <Buttons onClick={() => window.alert(alerts.m2)}>Módulo 2</Buttons>
+      </DivButtons>
     )
   }
 }
